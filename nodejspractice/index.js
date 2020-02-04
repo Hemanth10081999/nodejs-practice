0daa6f7c05d70c4a2830f858328cd07e6565c8b4
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-04 15:15:39 +0530

    nodejs custom modules

--- nodejspractice/index.js
+++ nodejspractice/index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var dt = require('./module')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('Hello World!'+dt.myDateTime());
 }).listen(8080);

f07c5159b136cf124694672c690f99b4c1afa95d
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-04 16:02:21 +0530

    url library practice

--- nodejspractice/index.js
+++ nodejspractice/index.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var dt = require('./module')
+var url = require('url');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!'+dt.myDateTime());
+  res.writeHead(200,{'Content-Type':'text/html'});
+  var q= url.parse(req.url,true).query
+  res.write(q.name+" "+q.age);
+  res.end();
 }).listen(8080);

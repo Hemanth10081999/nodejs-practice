e717d7c446e4394fcd2575fe775c66b26e9612f9
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-04 16:10:06 +0530

    read file example

--- nodejspractice/index.js
+++ nodejspractice/index.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var dt = require('./module')
-var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200,{'Content-Type':'text/html'});
-  var q= url.parse(req.url,true).query
-  res.write(q.name+" "+q.age);
-  res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', function (err, data) {
+        res.write(data);
+        res.end();
+    });
+
 }).listen(8080);

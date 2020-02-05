9246bfb1b896b3d7f38ab9a8beb8f7dad72a8ee2
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-05 18:09:49 +0530

    send mail code using nodejs

--- nodejspractice/index.js
+++ nodejspractice/index.js
@@ -1,23 +1,67 @@
-var events = require('events');
-var util = require('util');
+// // var nodemailer = require('nodemailer');
 
-var Person = function(name){
-    this.name=name;
-};
+// // var transporter = nodemailer.createTransport({
+// //     service : 'gmail',
+// //     auth : {
+// //         user : 'vhemantharajan@gmail.com',
+// //         pass : 'Alien106@Anna'
+// //     }
+// // });
 
-util.inherits(Person,events.EventEmitter)
+// // var mailoptions = {
+// //     from : 'vhemantharajan@gmail.com',
+// //     to : 'priyanjanari@gmail.com',
+// //     subject : 'testing mail from hemanth',
+// //     text : 'This is a system generated mail from nodejs kindly dont reply, reply panravan kaila kedacha sethan'
+// // };
 
-var Hemanth = new Person('Hemanth');
-var Amirtha = new Person('Amirtha');
-var Ari = new Person('Ari');
-var names = [Hemanth , Amirtha , Ari]
+// // transporter.sendMail(mailoptions, function(error,info){
+// //     if(error){
+// //         console.log(error);
+// //     }
+// //     else{
+// //         console.log('email sent '+ info.response);
+// //     }
+// // });
 
-names.forEach(function(person){
-    person.on('speak',function(msg){
-        console.log(person.name+" : "+msg)
-    });
+
+// var nodemailer = require('nodemailer');
+
+// var transporter = nodemailer.createTransport({
+    
+//         host: 'smtp.ethereal.email',
+//         port: 587,
+//         auth: {
+//             user: 'ethereal.user@ethereal.email',
+//             pass: 'verysecret'
+//         }
+// });
+
+// var mailOptions = {
+//   from: 'vhemantharajan@gmail.com',
+//   to: 'priyanjanhari@gmail.com',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
+
+
+
+const sendmail = require('sendmail')();
+ 
+sendmail({
+    to: 'vhemantharajan@gmail.com',
+    from: 'ceo@vugha.com',
+    subject: 'Mail testing',
+    html: 'eppadi, innaiku itha vachi fun pannituirrukom inga',
+  }, function(err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
 });
-
-Hemanth.emit('speak','hi guys how are you');
-Ari.emit('speak','i am fine');
-Amirtha.emit('speak','having fun ah');

6093b2e64ef312e050801d9f248b0a7c244bbb32
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-05 11:36:17 +0530

    psge directing

--- nodejspractice/index.js
+++ nodejspractice/index.js
@@ -1,27 +1,17 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+http.createServer(function(req,res){
     var q=url.parse(req.url,true)
-
-    res.write("host name : "+ q.search);
-    res.write("host name : "+ q.pathname);
-
-    var q1=q.query;
-    // res.write("  age is :"+q1.age);
-    // res.write( "  name is :"+q1.name);
-    res.end();
-}).listen(8080);
-
-
-// var url = require('url');
-// var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-// var q = url.parse(adr, true);
-
-// console.log(q.host); //returns 'localhost:8080'
-// console.log(q.pathname); //returns '/default.htm'
-// console.log(q.search); //returns '?year=2017&month=february'
-
-// var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-// console.log(qdata.month); //returns 'february'
+    var path="."+q.pathname
+    fs.readFile(path,function(err,data){
+        if(err){
+            res.writeHead(404,{'Content-Type':'text/html'})
+            return res.end("page not found");
+        }
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080)

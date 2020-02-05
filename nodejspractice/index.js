376a2374b70c7a73bef110f19a4563f81e891bef
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-05 11:20:56 +0530

    more on url library

--- nodejspractice/index.js
+++ nodejspractice/index.js
@@ -1,12 +1,27 @@
 var http = require('http');
-var dt = require('./module')
-var fs = require('fs');
+var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('index.html', function (err, data) {
-        res.write(data);
-        res.end();
-    });
+    var q=url.parse(req.url,true)
 
+    res.write("host name : "+ q.search);
+    res.write("host name : "+ q.pathname);
+
+    var q1=q.query;
+    // res.write("  age is :"+q1.age);
+    // res.write( "  name is :"+q1.name);
+    res.end();
 }).listen(8080);
+
+
+// var url = require('url');
+// var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+// var q = url.parse(adr, true);
+
+// console.log(q.host); //returns 'localhost:8080'
+// console.log(q.pathname); //returns '/default.htm'
+// console.log(q.search); //returns '?year=2017&month=february'
+
+// var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+// console.log(qdata.month); //returns 'february'

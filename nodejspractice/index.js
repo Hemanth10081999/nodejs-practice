4d9c3935c11476670c8910dc21f6102e30efad7b
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-06 18:10:12 +0530

    health api without express js

--- nodejspractice/index.js
+++ nodejspractice/index.js
@@ -1,7 +1,16 @@
-const os = require('os');
+const http = require('http')
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-console.log(`total memory : ${totalMemory}`);
-console.log(`free memory : ${freeMemory}`);
+const server = http.createServer((req, res) => {
+  console.log('url ' + req.url);
+  if (req.url == '/health') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('response from health api');
+    res.end();
+  }
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('page not found');
+    res.end();
+  }
+})
+server.listen(8080);

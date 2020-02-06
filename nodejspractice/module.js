02054f6e7ce249fab1b45847603f72de5f0060eb
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-06 12:39:44 +0530

    path module

--- nodejspractice/module.js
+++ nodejspractice/module.js
@@ -1,4 +1,11 @@
-exports.myDateTime=function(){
-    var dt=new Date();
-    return dt.getTime();
-};
+const  datetime=()=>{
+    // var dt=new Date();
+    console.log('sljh')
+    // return dt.getTime();
+}
+
+var date = 'date';
+
+module.exports.datetime=datetime;
+// module.exports.date=date;
+console.log(module)

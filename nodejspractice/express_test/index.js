31df5522707111b9d3c2bca4ec1cbaf37708799b
Hemantharajan V <Hemantharajan.V1@mrcooper.com>
2020-02-12 13:59:42 +0530

    weather api in local server

--- nodejspractice/express_test/index.js
+++ nodejspractice/express_test/index.js
@@ -1,37 +1,58 @@
 const express = require('express');
+const Axios = require('axios');
 const app = express();
-
-const users = [
-    {id:1,name:'hemanth'},
-    {id:2,name:'ari'},
-    {id:3,name:'amirtha'}
-];
+const cors = require('cors');
 
 
+app.use(cors({
+    origin:'http://localhost:3000'
+}));
 
 app.get('/', (req,res) => {
     res.send('Now its working!!!')
 });
 
-
-app.get('/api/users',(req,res)=>{
-    res.send(users);
+app.get('/api/weather/city/:city',async (req,res)=>{
+    res.setHeader('Content-Type','application/json');
+    var url= `http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=9328b94e7be4310a47cd459b2664068f&units=metric`
+    const data = await weathercall(url)
+    console.log("response sent using city name");
+    res.send(JSON.stringify(data));
+    
 })
 
-app.get('/api/users/:id',(req,res)=>{
-    const user = users.find(u => u.id=== parseInt(req.params.id))
-    if(!user){
-        res.status(404).send('invalid user')
+app.get('/api/weather/location/:lat/:lon',async (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    
+    var url= `http://api.openweathermap.org/data/2.5/weather?lat=${req.params.lat}&lon=${req.params.lon}&appid=9328b94e7be4310a47cd459b2664068f&units=metric`
+    const data = await weathercall(url)
+    console.log('response sent using location data');
+    res.send(JSON.stringify(data));
+})
+
+async function weathercall(url) {
+    // console.log(url);
+    var responsedata={}
+    await Axios.get(url)
+        .then(res=>{
+            responsedata=res.data
+        })        
+        .catch(error => {
+            if(error.response.status===404){
+                alert("enter correct data")
+                responsedata=error.response.status
+            }
+        });
+    if(responsedata===404){
+        return null;
     }
-    res.send(user)
-})
+    // console.log(responsedata);
+    return responsedata;
+}
 
-app.get('/api/users/:id/:session',(req,res)=>{
-    // res.send(req.query);
-    res.send(req.params);
-})
 
-const port=process.env.PORT||3000;
+
+const port=process.env.PORT||8080;
 app.listen(port, ()=>{
     console.log(`listening at port ${port}...`);
 });
